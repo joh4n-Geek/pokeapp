560f72698634e98c4ee526d7c31e87712cb86d64
jrojass <jrojass@correo.tdea.edu.co>
2024-03-20 08:30:00 -0500

    add logo, pagination and change some styles

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -15,11 +15,36 @@ import { EMPTY, Observable, catchError } from 'rxjs';
 export class PokemonListComponent implements OnInit {
   public pokemonResponse$!: Observable<PokemonData>;
   public showMessageError!: string;
+  previousText: string = '<< Prev';
+  nextText: string = 'Next >>';
+  nextPage: number = 0;
+  previousPage: number = 0;
 
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
     this.pokemonResponse$ = this.pokemonService.getPokemons().pipe(
+      catchError((error: string) => {
+        this.showMessageError = error;
+        return EMPTY;
+      })
+    );
+  }
+
+  paginationNext() {
+    this.nextPage = 16;
+    this.pokemonResponse$ = this.pokemonService.getPaginationNext(this.nextPage).pipe(
+      catchError((error: string) => {
+        this.showMessageError = error;
+        return EMPTY;
+      })
+    );
+  }
+
+  paginationPrevious() {
+    this.previousPage = 16;
+    // this.isDisabled = false;
+    this.pokemonResponse$ = this.pokemonService.getPaginationPrevious(this.previousPage).pipe(
       catchError((error: string) => {
         this.showMessageError = error;
         return EMPTY;
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,17 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { PokemonData } from '../interfaces/pokemon';
 import { environment } from '../../environments/environment.development';
+import { PokemonData } from '../interfaces/pokemon';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
-
+  limitPage: number = 16;
+  offsetPage: number = 0;
   constructor(private http: HttpClient) { }
 
   getPokemons(): Observable<PokemonData> {
-    return this.http.get<PokemonData>(`${environment.baseUrl}pokemon?limit=10&offset=0`);
+    return this.http.get<PokemonData>(`${environment.baseUrl}pokemon?limit=${this.limitPage}&offset=${this.offsetPage}`);
+  }
+
+  getPaginationNext(next: number) {
+    this.offsetPage = this.offsetPage + next;
+    return this.http.get<PokemonData>(`${environment.baseUrl}pokemon?limit=${this.limitPage}&offset=${this.offsetPage}`);
+  }
+
+  getPaginationPrevious(previous: number) {
+    this.offsetPage = this.offsetPage - previous;
+    return this.http.get<PokemonData>(`${environment.baseUrl}pokemon?limit=${this.limitPage}&offset=${this.offsetPage}`);
   }
 }
